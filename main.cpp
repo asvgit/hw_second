d56dedbcffb81f1a9f95054e8afe5b28105ab889
Арляпов Савелий <sava_arl@mail.ru>
2018-10-21 20:59:39 +0800

    build test

--- main.cpp
+++ main.cpp
@@ -10,14 +10,12 @@
 // ("11.", '.') -> ["11", ""]
 // (".11", '.') -> ["", "11"]
 // ("11.22", '.') -> ["11", "22"]
-std::vector<std::string> split(const std::string &str, char d)
-{
+std::vector<std::string> split(const std::string &str, char d) {
     std::vector<std::string> r;
 
     std::string::size_type start = 0;
     std::string::size_type stop = str.find_first_of(d);
-    while(stop != std::string::npos)
-    {
+    while(stop != std::string::npos) {
         r.push_back(str.substr(start, stop - start));
 
         start = stop + 1;
@@ -29,28 +27,21 @@ std::vector<std::string> split(const std::string &str, char d)
     return r;
 }
 
-int main(int argc, char const *argv[])
-{
-    try
-    {
+int main(int argc, char const *argv[]) {
+    try {
         std::vector<std::vector<std::string>> ip_pool;
 
-        for(std::string line; std::getline(std::cin, line);)
-        {
+        for(std::string line; std::getline(std::cin, line);) {
             std::vector<std::string> v = split(line, '\t');
             ip_pool.push_back(split(v.at(0), '.'));
         }
 
         // TODO reverse lexicographically sort
 
-        for(std::vector<std::vector<std::string> >::const_iterator ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
-        {
-            for(std::vector<std::string>::const_iterator ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part)
-            {
-                if (ip_part != ip->cbegin())
-                {
+        for(std::vector<std::vector<std::string> >::const_iterator ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip) {
+            for(std::vector<std::string>::const_iterator ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part) {
+                if (ip_part != ip->cbegin()) {
                     std::cout << ".";
-
                 }
                 std::cout << *ip_part;
             }
@@ -119,11 +110,8 @@ int main(int argc, char const *argv[])
         // 46.49.43.85
         // 39.46.86.85
         // 5.189.203.46
-    }
-    catch(const std::exception &e)
-    {
+    } catch(const std::exception &e) {
         std::cerr << e.what() << std::endl;
     }
-
     return 0;
 }
