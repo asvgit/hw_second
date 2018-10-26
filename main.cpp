788da2ea851dfc14be32693e6322bcda5d9e78ca
Арляпов Савелий <sava_arl@mail.ru>
2018-10-27 02:01:50 +0800

    some cleaning

--- main.cpp
+++ main.cpp
@@ -1,18 +1,19 @@
 #include <cassert>
 #include <cstdlib>
 #include <iostream>
+#include <iterator>
 #include <algorithm>
 #include <memory>
 #include "lib.h"
 
 int main() {
-    try {
-        std::vector<StringVector> ip_pool;
+	try {
+		std::vector<StringVector> ip_pool;
 
-        for(std::string line; std::getline(std::cin, line);) {
-            StringVector v = str::split(line, '\t');
-            ip_pool.push_back(str::split(v.at(0), '.'));
-        }
+		for(std::string line; std::getline(std::cin, line);) {
+			StringVector v = str::split(line, '\t');
+			ip_pool.push_back(str::split(v.at(0), '.'));
+		}
 
 		std::sort(ip_pool.begin(), ip_pool.end(), [](const auto &a, const auto &b) {
 			for(int i = 0; i < 4; ++i) {
@@ -24,19 +25,13 @@ int main() {
 			return true;
 		});
 
-		auto print_ip = [](const StringVector &ip) {
-            for(const auto &ip_part : ip) {
-                if (ip_part != ip[0]) {
-                    std::cout << ".";
-                }
-                std::cout << ip_part;
-            }
-            std::cout << std::endl;
+		auto print_ip = [](const auto &ip) {
+			std::copy(ip.begin(), ip.end(), std::ostream_iterator<string>(std::cout,"."));
+			std::cout << std::endl;
 		};
 
-        for(const auto &ip : ip_pool) {
+		for(const auto &ip : ip_pool)
 			print_ip(ip);
-        }
 
 		using filter_type = std::vector<std::shared_ptr<StringVector>>;
 		// filter_1 filter_46_70 filter_any_46; 
@@ -52,15 +47,14 @@ int main() {
 			}
 		}
 
-		std::cout << std::endl;
+		// std::cout << std::endl; // Debug
 		for (const auto &f : filters) {
-			for (const auto &item : f) {
+			for (const auto &item : f)
 				print_ip(*item);
-			}
-			std::cout << std::endl;
+			// std::cout << std::endl; // Debug
 		}
-    } catch(const std::exception &e) {
-        std::cerr << e.what() << std::endl;
-    }
-    return 0;
+	} catch(const std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
+	return 0;
 }
